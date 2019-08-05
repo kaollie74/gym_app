780b33f7bbb860a3782e428f405e0d2f6040b042
Kyle Olson <kaollie74@gmail.com>
2019-08-05 16:57:42 -0500

    capture values in state on Build Routine Page

--- src/components/BuildRoutine/BuildRoutine.js
+++ src/components/BuildRoutine/BuildRoutine.js
@@ -1,9 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { thisExpression } from '@babel/types';
 
 class BuildRoutinePage extends Component {
-  render(){
-    return(
-      <h1>Build Routine</h1>
+
+  state = {
+    routineName: '',
+    day: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('In Handle Submit');
+    this.props.dispatch({type: 'SET_ROUTINE_NAME', payload: this.state})
+
+  }
+
+
+
+  handleChange = (event, propsName) => {
+    this.setState({
+      [propsName]: event.target.value,
+
+    })
+  }
+
+
+  render() {
+
+    console.log('this is state', this.state)
+    return (
+      <>
+        <h1>Build Routine Name and Day</h1>
+
+        <form>
+          <input type='text' placeholder='Routine Name' onChange={(event) => this.handleChange(event, 'routineName')} />
+
+          <select onChange={(event) => this.handleChange(event, 'day')} >
+            <option value='default'>Select Day</option>
+            <option value="monday">Monday</option>
+            <option value="tuesday">Tuesday</option>
+            <option value="wednesday">Wednesday</option>
+            <option value="thursday">Thursday</option>
+            <option value="friday">Friday</option>
+            <option value="saturday">Saturday</option>
+            <option value="sunday">Sunday</option>
+          </select>
+          <br />
+          <button onClick={this.handleSubmit}>Submit</button>
+
+        </form>
+      </>
     )
   }
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { HashRouter as Router, Route, Redirect, Switch, } from 'react-router-dom';
+import '../App/App.css'
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 class HomePage extends Component {
 
-BuildARoutine = () => {
-  this.props.history.push('/build')
-}
+  BuildARoutine = () => {
+    this.props.history.push('/build')
+  }
 
-weeklyRoutine = () => {
-  this.props.history.push('/weekly')
+  weeklyRoutine = () => {
+    this.props.history.push('/weekly')
 
-}
+  }
 
-routineDatabase = () => {
-  this.props.history.push('/database')
-}
+  routineDatabase = () => {
+    this.props.history.push('/database')
+  }
 
   render() {
     return (
@@ -29,17 +30,21 @@ routineDatabase = () => {
             Welcome, {this.props.user.username}!
           </h1>
           <p>Your ID is: {this.props.user.id}</p>
+
+        </div>
+        <div>
+          <button className="buildRoutine" onClick={this.BuildARoutine}>Build A Routine</button>
+        </div>
+        <div>
+          <button className="weeklyRoutine" onClick={this.weeklyRoutine}>Weekly Routine</button>
+        </div>
+        <div>
+          <button className="routineDatabase" onClick={this.routineDatabase}>Routine Database</button>
+        </div>
+        <div>
           <LogOutButton className="log-in" />
         </div>
-        <div>
-          <button onClick={this.BuildARoutine}>Build A Routine</button>
-        </div>
-        <div>
-          <button onClick={this.weeklyRoutine}>Weekly Routine</button>
-        </div>
-        <div>
-          <button onClick={this.routineDatabase}>Routine Database</button>
-        </div>
+
       </>
 
     )
